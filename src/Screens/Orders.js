aea82f434048f46284a58e60a5362dbd928362a0
c0836175@mylambton.ca <c0836175@mylambton.ca>
2022-11-07 18:54:59 -0500

    Bug fixed

--- src/Screens/Orders.js
+++ src/Screens/Orders.js
@@ -1,13 +1,45 @@
-import { View, Text, FlatList, Image } from 'react-native'
+import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useSelector } from 'react-redux'
+import { useNavigation } from '@react-navigation/native'
 
 const Orders = () => {
+  const navigation = useNavigation();
   const orders = useSelector(state => state.OrderReducers);
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flex: 1 }}>
+      <View
+                    style={{
+                        width: '100%',
+                        height: 70,
+                        justifyContent: 'space-between',
+                        alignItems: 'center',
+                        flexDirection: 'row',
+                    }}>
+                    <TouchableOpacity
+                        style={{
+                            width:50,
+                            marginLeft: 30,
+                            justifyContent: 'center',
+                            alignItems:'flex-start',
+                            borderWidth: .2,
+                            padding: 7,
+                            borderRadius: 10,
+                            backgroundColor:'#ebe9e4'
+                        }}
+                        onPress={() => {
+                            navigation.goBack();
+                        }}
+                    >
+                        <Image source={require('../images/back.png')}
+                            style={{
+                                width: 24,
+                                height: 24,
+                            }} />
+                    </TouchableOpacity>
+                </View>
         <FlatList
           data={orders}
           renderItem={({ item, index }) => {
