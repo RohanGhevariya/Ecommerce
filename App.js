9262187cf1a12a5e3e8788fabafb56343ba9a934
Rohan <roh987gh@yahoo.com>
2022-11-09 18:18:12 -0500

    chanegs

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import AppNavigator from './src/AppNavigator'
+//import AppNavigator from './src/AppNavigator'
 import MainContainer from './src/MainContainer'
 import store from './src/redux/store/Store'
 import { Provider } from 'react-redux'
+import AppNavigator from './screens/AppNavigator'
 
 const App = () => {
   return (
